6417148053137d80d19fff17611e25018190d9f2
VIRMSKVLL <44964856+VIRMSKVLL@users.noreply.github.com>
2025-04-06 02:34:42 -0700

    fix attempt 1

--- src/display/graphics.cpp
+++ src/display/graphics.cpp
@@ -538,7 +538,7 @@ void ScreenScene::composite() {
   pp.startRender();
 
   glState.viewport.set(IntRect(0, 0, w, h));
-  if (!transparent && !shState->config().transparentDefault) // clear background to black unless transparent
+  if (!transparent || !shState->config().transparentDefault) // clear background to black unless transparent
     gl.ClearColor(0, 0, 0, 1);
   FBO::clear();
   if (!transparent) // restore clearcolor
